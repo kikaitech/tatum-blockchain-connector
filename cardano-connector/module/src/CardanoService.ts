9ecb9f2ca3d7ccc8880d752fa53f98fdffdd19b4
cuongnm <cuongnm@kikaitech.io>
2021-03-05 15:37:11 +0700

    add get block && transaction info by hash apis

--- cardano-connector/module/src/CardanoService.ts
+++ cardano-connector/module/src/CardanoService.ts
@@ -1,3 +1,4 @@
+import { Block, Transaction } from '@cardano-graphql/client-ts';
 import axios from 'axios';
 import { PinoLogger } from 'nestjs-pino';
 import { CardanoBlockchainInfo, WalletId } from './constants';
@@ -16,12 +17,21 @@ export abstract class CardanoService {
 
   protected abstract getCardanoWalletUrls(): Promise<string[]>;
 
+  public async getGraphQLEndpoint(): Promise<string> {
+    const [[url], port] = await Promise.all([
+      this.getNodesUrl(),
+      this.getCardanoGraphQLPort(),
+    ]);
+    return `${url}:${port}/graphql`;
+  }
+
   public async getBlockChainInfo(): Promise<CardanoBlockchainInfo> {
-    const testnet = await this.isTestnet();
-    const [url] = await this.getNodesUrl();
-    const port = await this.getCardanoGraphQLPort();
+    const [testnet, graphQLUrl] = await Promise.all([
+      this.isTestnet(),
+      this.getGraphQLEndpoint(),
+    ]);
     const { tip } = (
-      await axios.post(`${url}:${port}/graphql`, {
+      await axios.post(graphQLUrl, {
         query: '{ cardano { tip { number slotNo epoch { number } }} }',
       })
     ).data.data.cardano;
@@ -32,8 +42,63 @@ export abstract class CardanoService {
   }
 
   public async generateWallet(req: GenerateWalletMnemonic): Promise<WalletId> {
-    const [url] = await this.getNodesUrl();
-    const port = await this.getCardanoWalletPort();
+    const [[url], port] = await Promise.all([
+      this.getNodesUrl(),
+      this.getCardanoWalletPort(),
+    ])
     return (await axios.post(`${url}:${port}/v2/wallets`, req)).data.id;
+  }
+
+  public async getBlock(hash: string): Promise<Block> {
+    const graphQLUrl = await this.getGraphQLEndpoint();
+    const [block] = (
+      await axios.post(graphQLUrl, {
+        query: `{ blocks (where: { hash: { _eq: "${hash}" } }) {
+          fees
+          slotLeader { description, hash }
+          forgedAt
+          merkelRoot
+          number
+          opCert
+          slotInEpoch
+          slotNo
+          protocolVersion
+          size
+          transactionsCount
+          transactions { hash }
+          nextBlock { hash, number }
+          previousBlock { hash, number  }
+          vrfKey
+        } }`,
+      })
+    ).data.data.blocks;
+    return block;
+  }
+
+  public async getTransaction(hash: string): Promise<Transaction> {
+    const graphQLUrl = await this.getGraphQLEndpoint();
+    const [transaction] = (
+      await axios.post(graphQLUrl, {
+        query: `{ transactions (where: { hash: { _eq: "${hash}" } }) {
+          block { hash number }
+          blockIndex
+          deposit
+          fee
+          inputs { address sourceTxHash sourceTxIndex }
+          inputs_aggregate { aggregate { count } }
+          outputs { address index txHash value }
+          outputs_aggregate { aggregate { count }}
+          invalidBefore
+          invalidHereafter
+          mint { policyId }
+          size
+          totalOutput
+          includedAt
+          withdrawals { address amount transaction { hash }}
+          withdrawals_aggregate { aggregate { count } }
+        } }`,
+      })
+    ).data.data.transactions;
+    return transaction;
   }
 }
